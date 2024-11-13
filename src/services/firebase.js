cb3021929297f455f721d261a44f1f01fa663dc4
Kyla <kflmorcillos@gmail.com>
2024-11-13 09:11:33 +0800

    Initialize Firebase app

---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvWUUMOyITjPbJOSYXzhDhysjFRrSnepI",
+  authDomain: "ccisconcernhub.firebaseapp.com",
+  projectId: "ccisconcernhub",
+  storageBucket: "ccisconcernhub.firebasestorage.app",
+  messagingSenderId: "237012165084",
+  appId: "1:237012165084:web:347b3e3ceee20a41fcef19",
+  measurementId: "G-FEVT8R044D"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
