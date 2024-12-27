b13382abe32d92fac576e324e27d00d773e08f37
Kyla <kflmorcillos@gmail.com>
2024-12-28 00:06:04 +0800

    Add Concern class

---
+++ src/models/concern.js
@@ -0,0 +1,23 @@
+import Database from "../services/database";
+
+export default class Concern {
+    constructor({ attachmentLinks, category, creatorDisplayName, creatorUid, dateSubmitted, description, id, isResolved, isSpam, issueType, status, subject }) {
+        this.attachmentLinks = attachmentLinks;
+        this.category = category;
+        this.creatorDisplayName = creatorDisplayName;
+        this.creatorUid = creatorUid;
+        this.dateSubmitted = dateSubmitted.toDate();
+        this.description = description;
+        this.id = id;
+        this.isResolved = isResolved;
+        this.isSpam = isSpam;
+        this.issueType = issueType;
+        this.status = status;
+        this.subject = subject;
+    }
+
+    static async findById(concernId) {
+        const concernData = await Database.getConcernDoc(concernId);
+        return concernData ? new Concern(concernData) : null;
+    }
+}
