adeb1a0f917e03768d7f921f82989c6ed045bc3d
Kyla <kflmorcillos@gmail.com>
2024-12-28 17:20:36 +0800

    Refactor User class

--- src/models/user.js
+++ src/models/user.js
@@ -1,22 +1,4 @@
-import Database from "../services/database";
-
 export default class User {
-    static async findByUid(uid) {
-        const userData = await Database.getUserData(uid);
-        return userData ? new User(userData) : null;
-    }
-
-    static async new(uid, displayName) {
-        const userData = {
-            uid: uid,
-            displayName: displayName,
-            roles: ["student"],
-            assignedCategories: null,
-        };
-        await Database.setUserData(uid, userData);
-        return new User(userData);
-    }
-
     constructor({ uid, displayName, roles, assignedCategories = null }) {
         this.uid = uid;
         this.displayName = displayName;
