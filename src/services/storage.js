b8ac8c6d19c1310d4afa244a16f5c5ab90c4b0f1
Kyla <kflmorcillos@gmail.com>
2024-12-31 23:09:11 +0800

    Add Storage class

---
+++ src/services/storage.js
@@ -0,0 +1,42 @@
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from "./firebase";
+
+export default class Storage {
+    static async uploadFile(file, path) {
+        const storageRef = ref(storage, path);
+        await uploadBytes(storageRef, file);
+        console.log("File uploaded to", path);
+    }
+
+//     static async uploadFile(file, path) {
+//         return new Promise((resolve, reject) => {
+//             const storageRef = ref(storage, path);
+//             const uploadTask = uploadBytesResumable(storageRef, file);
+//
+//             uploadTask.on(
+//                 "state_changed",
+//                 (snapshot) => {
+//                     // Progress function (optional)
+//                 },
+//                 (error) => {
+//                     // Error function
+//                     reject(error);
+//                 },
+//                 async () => {
+//                     // Complete function
+//                     const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+//                     resolve(downloadURL);
+//                 }
+//             );
+//         });
+//     }
+//
+//     static async uploadFiles(files, path) {
+//         const uploadPromises = files.map((file, index) => {
+//             const filePath = `${path}/${file.name}`;
+//             return this.uploadFile(file, filePath);
+//         });
+//
+//         return Promise.all(uploadPromises);
+//     }
+}
