0e5e778f713fcaa35f3b626208a531a70e2a475e
Kyla <kflmorcillos@gmail.com>
2025-01-02 02:37:30 +0800

    Implement LoadingButton

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,5 +1,6 @@
 export default function LoadingSpinner({
     size = 24,
+    stroke = "#686868",
     className = "",
     ...props
 }) {
@@ -11,7 +12,7 @@ export default function LoadingSpinner({
             {...props}
             viewBox="0 0 24 24"
             fill="none"
-            stroke="#686868"
+            stroke={stroke}
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
@@ -20,4 +21,17 @@ export default function LoadingSpinner({
             <path d="M21 12a9 9 0 1 1-6.219-8.56" />
         </svg>
     );
+}
+
+export function LoadingButton({
+    loadingState,
+    spinnerStroke = "#686868",
+    ...props
+}) {
+    return <button {...props}>{
+        !loadingState ? props.children : <>
+            <LoadingSpinner stroke={spinnerStroke} className="inline mr-2" />
+            {props.children}
+        </>
+    }</button>;
 }
