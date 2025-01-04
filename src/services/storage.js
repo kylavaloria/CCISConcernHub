5de33987b6f81fbcd8e4e331d3a2aea7ad60b4fa
Kyla <kflmorcillos@gmail.com>
2025-01-04 08:55:18 +0800

    Storage: Remove comments

--- src/services/storage.js
+++ src/services/storage.js
@@ -7,36 +7,4 @@ export default class Storage {
         await uploadBytes(storageRef, file);
         return await getDownloadURL(storageRef);
     }
-
-//     static async uploadFile(file, path) {
-//         return new Promise((resolve, reject) => {
-//             const storageRef = ref(storage, path);
-//             const uploadTask = uploadBytesResumable(storageRef, file);
-//
-//             uploadTask.on(
-//                 "state_changed",
-//                 (snapshot) => {
-//                     // Progress function (optional)
-//                 },
-//                 (error) => {
-//                     // Error function
-//                     reject(error);
-//                 },
-//                 async () => {
-//                     // Complete function
-//                     const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-//                     resolve(downloadURL);
-//                 }
-//             );
-//         });
-//     }
-//
-//     static async uploadFiles(files, path) {
-//         const uploadPromises = files.map((file, index) => {
-//             const filePath = `${path}/${file.name}`;
-//             return this.uploadFile(file, filePath);
-//         });
-//
-//         return Promise.all(uploadPromises);
-//     }
 }
