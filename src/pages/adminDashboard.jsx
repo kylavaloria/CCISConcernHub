c66a64ad064828accd2e8bbf81dc4a25584071ea
Kyla <kflmorcillos@gmail.com>
2025-01-06 00:24:34 +0800

    Use real data on fetching user concerns on Admin Dashboard

--- src/pages/adminDashboard.jsx
+++ src/pages/adminDashboard.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState, useRef, useCallback } from 'react';
 import Footer from '../components/footer';
 import ConcernList from '../components/concernList';
 import DashboardStats from '../components/dashboardStats';
+import Database, { Pagination } from '../services/database';
 
-export function AdminDashboard() {
+export function AdminDashboard({ userData }) {
+    const [concerns, setConcerns] = useState([]);
+    const pagination = useRef(new Pagination());
+
+    const fetchUserConcerns = useCallback(async () => {
+        if (userData) {
+            const userConcerns = await Database.getUserConcerns(userData.uid, pagination.current);
+
+            if (concerns.length === 0) {
+                setConcerns(userConcerns);
+            } else {
+                setConcerns((prevConcerns) => [...prevConcerns, ...userConcerns]);
+            }
+        }
+    }, [userData, concerns]);
+
+    useEffect(() => {
+        fetchUserConcerns();
+    }, [fetchUserConcerns]);
+
     // Dummy metrics data
     const metricsData = {
         open: 5,
@@ -13,35 +34,13 @@ export function AdminDashboard() {
         totalConcerns: 21,
     };
 
-    // Dummy concern data
-    const concerns = [
-        {
-            id: 1,
-            issueType: 'Concern',
-            category: 'Website',
-            subject: 'Website is down',
-            status: 'Closed',
-            dateSubmitted: '2024-10-20',
-            studentName: 'John Doe'
-        },
-        {
-            id: 2,
-            issueType: 'Complaint',
-            category: 'Registration',
-            subject: 'Late Registration Issue',
-            status: 'In Progress',
-            dateSubmitted: '2024-10-21',
-            studentName: 'Jane Smith'
-        },
-    ];
-
     return (
         <div>
             <main className="container mx-auto p-4">
                 <h2 className="text-2xl font-semibold mt-2 text-blue-400">Concern Overview</h2>
                 <DashboardStats metrics={metricsData} />
                 <h2 className="text-xl font-semibold mt-6 text-blue-400">Manage Concerns</h2>
-                <ConcernList concerns={concerns} />
+                <ConcernList concerns={concerns} fetchUserConcerns={fetchUserConcerns} />
             </main>
             <Footer />
         </div>
