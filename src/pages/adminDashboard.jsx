7a4b1bd7d74c67634867e616b82b1c0d7b54024a
Katherine Quijano <katachiequijano984@gmail.com>
2025-01-12 21:12:36 +0800

    Updated Filtering queries

--- src/pages/adminDashboard.jsx
+++ src/pages/adminDashboard.jsx
@@ -5,46 +5,39 @@ import DashboardStats from '../components/dashboardStats';
 import Database, { Pagination } from '../services/database';
 import LoadingSpinner from '../components/loading';
 
+//fetching concerns from the database
 export function AdminDashboard({ userData }) {
     const [concerns, setConcerns] = useState(undefined);
     const pagination = useRef(new Pagination(5));
-
     const fetchCategoryConcerns = useCallback(async () => {
         if (userData) {
-            const categoryConcerns = await Database.getCategoryConcerns(userData.assignedCategories, pagination.current)
-
-            if (concerns === undefined) {
+            const categoryConcerns = await Database.getCategoryConcerns(userData.assignedCategories, pagination.current);
+            if (!concerns) {
                 setConcerns(categoryConcerns);
             } else {
                 setConcerns([...concerns, ...categoryConcerns]);
             }
         }
     }, [userData, concerns]);
-
     useEffect(() => {
         fetchCategoryConcerns();
     }, [fetchCategoryConcerns]);
-
-    // Dummy metrics data
-    const metricsData = {
-        open: 5,
-        inProgress: 3,
-        onHold: 2,
-        resolved: 10,
-        unresolved: 1,
-        totalConcerns: 21,
-    };
 
     return (
         <div>
             <main className="container mx-auto p-4">
                 <h2 className="text-2xl font-semibold mt-2 text-blue-400">Concern Overview</h2>
-                <DashboardStats metrics={metricsData} />
+                <DashboardStats concerns={concerns} />
                 <h2 className="text-xl font-semibold mt-6 text-blue-400">Manage Concerns</h2>
-                {
-                    concerns === undefined ? <LoadingSpinner /> :
-                    <ConcernList userData={userData} concerns={concerns} fetchUserConcerns={fetchCategoryConcerns} />
-                }
+                {concerns === undefined ? (
+                    <LoadingSpinner />
+                ) : (
+                    <ConcernList
+                        userData={userData}
+                        concerns={concerns}
+                        fetchUserConcerns={fetchCategoryConcerns}
+                    />
+                )}
             </main>
             <Footer />
         </div>
